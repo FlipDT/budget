4ef6a2339ada20e6e518cf068f904db9076f9421
FlipDT <arthur.laprie@gmail.com>
2024-04-18 19:23:45 +0200

    fix(backend): Edit category entity

--- backend/src/budget/entities/category.entity.ts
+++ backend/src/budget/entities/category.entity.ts
@@ -1,13 +1,16 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Operation } from "./operation.entity";
+import { IsNotEmpty } from "class-validator";
 
 @Entity()
 export class Category{
 
 @PrimaryGeneratedColumn()
+@IsNotEmpty()
 id: number;
 
 @Column()
+@IsNotEmpty()
 name: string;
 
 @OneToMany(() => Operation, (operation) => operation.category)
