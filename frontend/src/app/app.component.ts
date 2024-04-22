ea878473a16e8114534d6454688e41d6447f8b58
FlipDT <arthur.laprie@gmail.com>
2024-04-22 18:28:34 +0200

    feat(app): finish app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, CommonModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend';
+  showMenu: boolean = true;
+  username: string = '';
+  //constructor(private apiService: ApiService) {}
 }
+
+// ngOnInit() {
+//   this.apiService.jwtUserToken.subscribe((token) => {
+//     if (token) {
+//       const decoded: any = jwtDecode(token);
+//       this.username = decoded.username;
+//     }
+
+//     if (this.username) {
+//       this.showMenu = false;
+//     } else {
+//       this.showMenu = true;
+//     }
+//   });
+// }
+
+// logout() {
+//   this.username = '';
+//   this.username = this.apiService.logout();
+// }
