66d396d1f12d366c2c9c8b9eb53ddf0ed3a9e80f
FlipDT <arthur.laprie@gmail.com>
2024-04-24 10:34:00 +0200

    fix(home): add difference between positive and negative number

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -34,8 +34,12 @@ export class HomeComponent {
   startDate: Date | null = null;
   entriesAndExpenses: Operation[] = [];
   operations: any[] = [];
+  entries: any[] = [];
+  expenses: any[] = []
 
-  constructor(private http: HttpClient, private apiService: ApiService) {}
+  constructor(private http: HttpClient, private apiService: ApiService) {
+    this.fetchDataFromBackend();
+  }
 
   onDateSelected(event: MatDatepickerInputEvent<Date>) {
     this.startDate = event.value;
@@ -47,17 +51,28 @@ export class HomeComponent {
     this.entriesAndExpenses.push(operation);
   }
 
+  splitOperations(operations: any[]) {
+    const entries = operations.filter(op => op.amount >= 0);
+    const expenses = operations.filter(op => op.amount < 0);
+    return { entries, expenses };
+  }
+
+  // Appeler cette méthode lorsque les données sont récupérées
   fetchDataFromBackend(): void {
     this.apiService.getAllOperations().subscribe(
-      (data: any) => {
-        this.operations = data;
-        console.log('Données récupérées depuis le backend :', this.operations);
+      (data: any[]) => {
+        // Diviser les opérations en entrées et dépenses
+        const { entries, expenses } = this.splitOperations(data);
+        
+        // Assigner les entrées et dépenses aux propriétés correspondantes
+        this.entries = entries;
+        this.expenses = expenses;
+
+        console.log('Entrées:', this.entries);
+        console.log('Dépenses:', this.expenses);
       },
       (error) => {
-        console.error(
-          "Une erreur s'est produite lors de la récupération des données:",
-          error
-        );
+        console.error("Une erreur s'est produite lors de la récupération des données:", error);
       }
     );
   }
