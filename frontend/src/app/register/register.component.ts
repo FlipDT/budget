0acb0833d918da32834c9cc560b6a7acb4cbe598
FlipDT <arthur.laprie@gmail.com>
2024-04-24 16:08:13 +0200

    feat(register): create register method and logic

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ApiService } from '../services/api.services';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  constructor(private apiService: ApiService) {}
 
-}
+  register(registerForm: NgForm) {
+    if (registerForm.invalid) {
+      return;
+    }
+
+    this.apiService.register(registerForm.value).subscribe((res) => {
+        console.log(res);
+      });
+    }
+  }
+
