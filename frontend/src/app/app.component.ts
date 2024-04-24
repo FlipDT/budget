e2dabdf350c833eba5c7d37b79fffdaf80945af8
FlipDT <arthur.laprie@gmail.com>
2024-04-24 10:32:55 +0200

    fix(app): add logout to html

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { ApiService } from './services/api.services';
+import { jwtDecode } from 'jwt-decode';
 //import { ApiService } from './services/api.services';
 
 @Component({
@@ -16,25 +18,25 @@ export class AppComponent {
   title = 'frontend';
   showMenu: boolean = true;
   username: string = '';
-  constructor(/*private apiService: ApiService */) {}
+  constructor(private apiService: ApiService) {}
 
-  // logout() {
-  //   this.username = '';
-  //   this.username = this.apiService.logout();
-  // }
+  ngOnInit() {
+    this.apiService.jwtUserToken.subscribe((token) => {
+      if (token) {
+        const decoded: any = jwtDecode(token);
+        this.username = decoded.username;
+      }
+
+      if (this.username) {
+        this.showMenu = false;
+      } else {
+        this.showMenu = true;
+      }
+    });
+  }
+
+  logout() {
+    this.username = '';
+    this.username = this.apiService.logout();
+  }
 }
-
-// ngOnInit() {
-//   this.apiService.jwtUserToken.subscribe((token) => {
-//     if (token) {
-//       const decoded: any = jwtDecode(token);
-//       this.username = decoded.username;
-//     }
-
-//     if (this.username) {
-//       this.showMenu = false;
-//     } else {
-//       this.showMenu = true;
-//     }
-//   });
-// }
