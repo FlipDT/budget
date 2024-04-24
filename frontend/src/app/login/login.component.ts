05762e50a75de5eeae6852c9241ef3d88c0fbcc7
FlipDT <arthur.laprie@gmail.com>
2024-04-24 10:30:19 +0200

    feat(login): create login method

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ApiService } from '../services/api.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private apiService: ApiService, private router: Router) {}
 
+  ngOnInit(): void {
+    this.apiService.jwtUserToken.subscribe((token) => {
+      if (token) {
+        this.router.navigateByUrl('/').then();
+      }
+    });
+  }
+
+  login(loginForm: NgForm) {
+    if (loginForm.invalid) {
+      return;
+    }
+    const { username, password } = loginForm.value;
+    this.apiService.login(username, password);
+    return loginForm.reset();
+  }
 }
