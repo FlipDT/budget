f0331cc10637cbd4fa1022d708a8b17c21288f19
FlipDT <arthur.laprie@gmail.com>
2024-04-26 00:29:11 +0200

    fix(login): fix login style, logic and method

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Validators } from '@angular/forms';
 import { ApiService } from '../services/api.services';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(private apiService: ApiService, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {}
+
+  form = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
 
   ngOnInit(): void {
     this.apiService.jwtUserToken.subscribe((token) => {
@@ -21,12 +41,16 @@ export class LoginComponent {
     });
   }
 
-  login(loginForm: NgForm) {
-    if (loginForm.invalid) {
+  login() {
+    if (this.form.invalid) {
       return;
     }
-    const { username, password } = loginForm.value;
+    const { username, password } = this.form.value;
+
+    if (!username || !password) {
+      return;
+    }
+
     this.apiService.login(username, password);
-    return loginForm.reset();
   }
 }
