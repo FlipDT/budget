3950e2961adb8b7da2cdc6a658dd02414a826de4
FlipDT <arthur.laprie@gmail.com>
2024-04-26 00:32:21 +0200

    feat(app): add MatDialog and Charts providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {
+  provideCharts,
+  withDefaultRegisterables,
+  } from 'ng2-charts';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {
@@ -10,12 +13,18 @@ import {
 } from '@angular/common/http';
 import { baseUrl } from './services/base-url.interceptor';
 import { provideToastr } from 'ngx-toastr';
+import {
+  MAT_DIALOG_DEFAULT_OPTIONS,
+  MatDialog,
+} from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideCharts(withDefaultRegisterables()),
     provideRouter(routes),
     provideAnimationsAsync(),
     provideToastr(),
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
     provideHttpClient(withFetch(), withInterceptors([baseUrl])),
   ],
 };
